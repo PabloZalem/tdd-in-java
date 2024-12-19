5a97935bb0ea190ef96f6f0fc31d565af1bd64f0
PabloZalem <zalem.pablo@yahoo.com>
2024-12-18 21:24:58 -0300

    create: method for enqueue element and refactor code

--- queue/src/test/java/QueueTest.java
+++ queue/src/test/java/QueueTest.java
@@ -1,12 +1,25 @@
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class QueueTest {
+    Queue queue;
+
+    @Before
+    public void setup() {
+        queue = new Queue();
+    }
 
     @Test
     public void createQueue() {
-        var queue = new Queue();
         assertTrue(queue.isEmpty());
+    }
+
+    @Test
+    public void enqueueElement() {
+        queue.enqueue(0);
+        assertFalse(queue.isEmpty());
     }
 }
