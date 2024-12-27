5d02815ecabf7e65112b3f0bc2b98f2464ea0266
PabloZalem <zalem.pablo@yahoo.com>
2024-12-27 18:43:52 -0300

    create: queue with tdd

---
+++ queue/src/main/java/Queue.java
@@ -0,0 +1,32 @@
+public class Queue {
+
+    private int size = 0;
+    private int[] elements = new int[2];
+    private int contador = 0;
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void enqueue(int element) {
+        if (size >= elements.length) throw new OverflowException();
+        contador = 0;
+        this.elements[size++] = element;
+    }
+
+    public int dequeue() {
+        if (size == 0) throw new UnderflowException();
+        size--;
+        return elements[contador++];
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public class UnderflowException extends RuntimeException{
+    }
+
+    public class OverflowException extends RuntimeException{
+    }
+}
