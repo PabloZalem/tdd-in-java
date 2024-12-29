82581079aa1f7f35c9fc211f9ce890b6fde326d7
PabloZalem <zalem.pablo@yahoo.com>
2024-12-29 17:26:51 -0300

    create: factoresPrime

--- fatorial recursivo/src/test/java/Factorial.java
+++ fatorial recursivo/src/test/java/Factorial.java
@@ -4,20 +4,16 @@ import static org.junit.Assert.assertEquals;
 
 public class Factorial {
     @Test
-    public void factorial(){
+    public void factorial() {
         assertEquals(1, factor(0));
         assertEquals(1, factor(1));
         assertEquals(2, factor(2));
         assertEquals(6, factor(3));
         assertEquals(24, factor(4));
-        assertEquals(120, factor(5));
     }
 
     private int factor(int n) {
         if (n < 2) return 1;
-
-        if (n == 2) return n;
-
         return factor(n - 1) * n;
     }
 }
